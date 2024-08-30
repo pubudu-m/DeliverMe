1a85d88538b37f42870fb5961fc3d15aff436f19
Pubudu Mihiranga <i.pubudumihiranga@gmail.com>
2024-08-30 12:03:11 +0530

    initial project setup without storyboards

---
+++ DeliverMe/Screens/MyDeliveriesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  DeliverMe
+//
+//  Created by Pubudu Mihiranga on 2024-08-30.
+//
+
+import UIKit
+
+class MyDeliveriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
+
